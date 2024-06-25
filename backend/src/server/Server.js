30990103f9f783946ae9a8ec029477c03d0128d7
fabiodplacerda <lacerda96@gmail.com>
2024-06-25 19:21:55 +0100

    feat: Enable CORS for the server

--- backend/src/server/Server.js
+++ backend/src/server/Server.js
@@ -23,6 +23,7 @@ export default class Server {
       console.log(`Server in running on http://${this.#host}:${this.#port}`);
     });
     this.#app.use(express.json());
+    this.#app.use(cors());
     this.#router.forEach(route => {
       this.#app.use(route.getRouteStartingPoint(), route.getRouter());
     });
