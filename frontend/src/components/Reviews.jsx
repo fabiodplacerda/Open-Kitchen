8849d4cc5ff42c220007d7831f0f1bd611553879
fabiodplacerda <lacerda96@gmail.com>
2024-06-26 21:58:31 +0100

    feat: Add ability for users to post reviews for recipes

--- frontend/src/components/Reviews.jsx
+++ frontend/src/components/Reviews.jsx
@@ -1,14 +1,52 @@
-import { useEffect, useState } from "react";
-import { getReviews } from "../services/review.service";
+import { useContext, useEffect, useState } from "react";
+import { createReview, getReviews } from "../services/review.service";
+import { Button, Rating, Typography } from "@mui/material/";
+import AddCommentIcon from "@mui/icons-material/AddComment";
+import { UserContext } from "../context/UserContext";
 
 const Reviews = ({ recipeId }) => {
+  const { loggedUser } = useContext(UserContext);
   const [reviews, setReviews] = useState([]);
+  const [rating, setRating] = useState(1);
+  const [body, setBody] = useState("");
+  const [isRating, setIsRating] = useState(false);
+
+  const postNewReview = async () => {
+    const newReview = {
+      rating,
+      body,
+      author: loggedUser.user._id,
+      recipeId,
+    };
+    try {
+      const newReviewResponse = await createReview(
+        recipeId,
+        newReview,
+        loggedUser.userToken
+      );
+
+      console.log(newReviewResponse.newReview);
+
+      if (newReviewResponse && newReviewResponse.newReview) {
+        console.log(newReviewResponse.newReview);
+
+        setReviews((prevReviews) => [
+          newReviewResponse.newReview,
+          ...prevReviews,
+          ,
+        ]);
+        setBody("");
+        setRating(1);
+        setIsRating(false);
+      }
+    } catch (e) {}
+  };
 
   const getReviewsData = async () => {
     try {
       const reviewsData = await getReviews(recipeId);
       console.log(reviewsData);
-      setReviews(reviewsData.reviews);
+      setReviews(reviewsData.reviews.reverse());
     } catch (e) {}
   };
 
@@ -23,11 +61,51 @@ const Reviews = ({ recipeId }) => {
           one!
         </p>
       )}
+
+      {!isRating && loggedUser && (
+        <Button
+          variant="contained"
+          color="success"
+          startIcon={<AddCommentIcon />}
+          onClick={() => setIsRating(true)}
+        >
+          Add a review
+        </Button>
+      )}
+
+      {isRating && (
+        <form action="">
+          <Typography component="legend">Rating</Typography>
+          <Rating
+            value={rating}
+            onChange={(event, newValue) => {
+              setRating(newValue);
+            }}
+          />
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+          <Button variant="contained" color="primary" onClick={postNewReview}>
+            Submit review
+          </Button>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() => setIsRating(false)}
+          >
+            Cancel
+          </Button>
+        </form>
+      )}
       {reviews.map((review) => {
         return (
-          <div className="card">
+          <div className="card" key={review._id}>
             <p>Author: {review.author.username}</p>
-            <p>Rating: {review.rating}</p>
+            <Rating value={review.rating} readOnly />
             <p>{review.body}</p>
           </div>
         );
--- frontend/src/services/review.service.js
+++ frontend/src/services/review.service.js
@@ -1,12 +1,27 @@
 import axios from "axios";
 
 export const getReviews = async (recipeId) => {
-  console.log(recipeId);
   try {
     const response = await axios.get(
       `http://localhost:3000/recipe/${recipeId}/reviews`
     );
 
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const createReview = async (recipeId, newReview, token) => {
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  try {
+    const response = await axios.post(
+      `http://localhost:3000/recipe/${recipeId}/createReview`,
+      newReview,
+      { headers }
+    );
     return response.data;
   } catch (e) {
     console.log(e);
