88167138b536fc0b0014e2e1936f92107a28b662
fabiodplacerda <lacerda96@gmail.com>
2024-06-29 12:18:42 +0100

    refactor: seed functionality

--- backend/seed/seed.js
+++ backend/seed/seed.js
@@ -6,23 +6,30 @@ import Review from '../src/models/review.model.js';
 import reviewsData from './data/reviewsData.js';
 import recipesData from './data/recipesData.js';
 import userData from './data/userData.js';
+import UserServices from '../src/services/User.service.js';
 
 Config.loadConfig();
+const userService = new UserServices();
 
 const { URI } = process.env;
 const database = new Database(URI);
-database.connect();
 
-try {
-  await User.deleteMany();
-  await Recipe.deleteMany();
-  await Review.deleteMany();
-  await User.insertMany(userData);
-  await Recipe.insertMany(recipesData);
-  await Review.insertMany(reviewsData);
-  console.log('Database was seeded');
-} catch (e) {
-  console.log(`Error seeding the database: ${e.message}`);
-} finally {
-  database.disconnect();
-}
+const run = async () => {
+  try {
+    await database.connect();
+    await User.deleteMany();
+    await Recipe.deleteMany();
+    await Review.deleteMany();
+
+    await Promise.all(userData.map(user => userService.createAccount(user)));
+    await Recipe.insertMany(recipesData);
+    await Review.insertMany(reviewsData);
+    console.log('Database was seeded');
+  } catch (e) {
+    console.log(`Error seeding the database: ${e.message}`);
+  } finally {
+    database.disconnect();
+  }
+};
+
+await run();
